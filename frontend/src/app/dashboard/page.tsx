e60dd1fa66293162a60b1ce574c70e043ddb774f
CYRTRUS <totae9949@gmail.com>
2026-09-09 20:48:07 +0700

    Initialize with Header

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
-export function Dashboard() {
-  return {}
+export default function Dashboard() {
+  return (
+    <div>
+      <header className="h-[80px] w-full bg-[#C4DFAA] flex items-center pl-[30px] text-black text-[40px] font-bold">
+        Dashboard
+      </header>
+
+      <main>
+        <p>Hello!</p>
+      </main>
+    </div>
+  );
 }
