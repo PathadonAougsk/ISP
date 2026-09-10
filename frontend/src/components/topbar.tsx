5656bcbc04fb218708172bfa8aedf4d7fab9f9ec
CYRTRUS <totae9949@gmail.com>
2026-09-10 16:07:22 +0700

    Update page layout and color declaring

--- frontend/src/components/topbar.tsx
+++ frontend/src/components/topbar.tsx
@@ -1,20 +1,23 @@
-"use client"
+"use client";
+
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation"
+import { usePathname } from "next/navigation";
 
 export default function Topbar() {
   const pathname = usePathname().replace("/", "");
+
   return (
-    <div className="w-full h-15 flex flex-row bg-[#C4DFAA] sticky top-0 z-10 items-center justify-between p-3">
-      <h1 className="font-semibold text-3xl">{pathname[0].toUpperCase() + pathname.slice(1)}</h1>
-      <div className="flex flex-row gap-2 text-center items-center justify-center">
-        <Image src={"./profile.svg"} width={40} height={40} alt="Profile"></Image>
-        <div className="hidden md:flex flex-row gap-2 items-center">
-          <h1 className="font-semibold">Pasin Mclearn</h1>
-          <h1 className="font-semibold">|</h1>
-          <h1 className="font-semibold">Lab Member</h1>
+    <div className="sticky top-0 z-10 flex h-15 w-full flex-row items-center justify-between bg-(--primary-color-3) p-7.5">
+      <h1 className="text-3xl font-bold">{pathname[0].toUpperCase() + pathname.slice(1)}</h1>
+
+      <div className="flex flex-row items-center justify-center gap-5 text-center">
+        <Image src="./profile.svg" width={36} height={36} alt="Profile" />
+        <div className="hidden flex-row items-center gap-2 text-lg md:flex">
+          <h1 className="font-bold">Pasin Mclearn</h1>
+          <h1 className="font-bold">|</h1>
+          <h1 className="font-bold">Lab Member</h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
