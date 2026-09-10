8ef5dd589d5642ecba9cb6807f6661e13af806ce
CYRTRUS <totae9949@gmail.com>
2026-09-10 23:43:06 +0700

    Fix

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-"use client";
-
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function Sidebar() {
   const pathname = usePathname().replace("/", "");
-  const icons = ["dashboard", "request-table", "report"];
   const icons = ["dashboard", "request-table", "report"];
   const router = useRouter();
 
@@ -18,37 +14,37 @@ export default function Sidebar() {
         <div className="flex h-15 w-full select-none items-center justify-center hover:bg-(--primary-color-2-hover)">
           <Image src="./sidebar.svg" width={20} height={20} alt="Sidebar" draggable={false} /></div>
 
-    <div className="sticky top-0 left-0 flex h-screen w-15 shrink-0 flex-col justify-between bg-(--primary-color-2)">
-      <div className="flex h-fit w-full flex-col items-center justify-evenly">
-        <div className="flex h-15 w-full select-none items-center justify-center hover:bg-(--primary-color-2-hover)">
-          <Image src="./sidebar.svg" width={20} height={20} alt="Sidebar" draggable={false} /></div>
+        <div className="sticky top-0 left-0 flex h-screen w-15 shrink-0 flex-col justify-between bg-(--primary-color-2)">
+          <div className="flex h-fit w-full flex-col items-center justify-evenly">
+            <div className="flex h-15 w-full select-none items-center justify-center hover:bg-(--primary-color-2-hover)">
+              <Image src="./sidebar.svg" width={20} height={20} alt="Sidebar" draggable={false} /></div>
 
-        {icons.map((urlName) =>
-          pathname === urlName ? (
-            <div key={urlName} className="flex h-15 w-full select-none items-center justify-center bg-(--primary-color-1) hover:bg-(--primary-color-1-hover)">
-              <Image src={`./${urlName}.svg`} width={20} height={20} alt={urlName} draggable={false} /></div>
-          ) : (
-            <div key={urlName} className="flex h-15 w-full select-none items-center justify-center hover:bg-(--primary-color-2-hover)" onClick={() => router.replace(`/${urlName}`)}>
-              <Image src={`./${urlName}.svg`} width={20} height={20} alt={urlName} draggable={false} /></div>
-          )
-        )}
-      </div>
+            {icons.map((urlName) =>
+              pathname === urlName ? (
+                <div key={urlName} className="flex h-15 w-full select-none items-center justify-center bg-(--primary-color-1) hover:bg-(--primary-color-1-hover)">
+                  <Image src={`./${urlName}.svg`} width={20} height={20} alt={urlName} draggable={false} /></div>
+              ) : (
+                <div key={urlName} className="flex h-15 w-full select-none items-center justify-center hover:bg-(--primary-color-2-hover)" onClick={() => router.replace(`/${urlName}`)}>
+                  <Image src={`./${urlName}.svg`} width={20} height={20} alt={urlName} draggable={false} /></div>
+              )
+            )}
+          </div>
 
-      <div className="flex h-fit w-full flex-col items-center justify-evenly select-none">
-        <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
-          <Image src="./announcement.svg" width={20} height={20} alt="Announcement" draggable={false} /></div>
-        <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
-          <Image src="./setting.svg" width={20} height={20} alt="Setting" draggable={false} /></div>
-      </div>
-      </div>
+          <div className="flex h-fit w-full flex-col items-center justify-evenly select-none">
+            <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
+              <Image src="./announcement.svg" width={20} height={20} alt="Announcement" draggable={false} /></div>
+            <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
+              <Image src="./setting.svg" width={20} height={20} alt="Setting" draggable={false} /></div>
+          </div>
+        </div>
 
-      <div className="flex h-fit w-full flex-col items-center justify-evenly select-none">
-        <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
-          <Image src="./announcement.svg" width={20} height={20} alt="Announcement" draggable={false} /></div>
-        <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
-          <Image src="./setting.svg" width={20} height={20} alt="Setting" draggable={false} /></div>
+        <div className="flex h-fit w-full flex-col items-center justify-evenly select-none">
+          <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
+            <Image src="./announcement.svg" width={20} height={20} alt="Announcement" draggable={false} /></div>
+          <div className="flex h-15 w-full items-center justify-center hover:bg-(--primary-color-2-hover)">
+            <Image src="./setting.svg" width={20} height={20} alt="Setting" draggable={false} /></div>
+        </div>
       </div>
     </div>
-  );
   );
 }
